14eb1736c37c739f64988fae1e37f7395c1e0e72
qeqe6789 <85107088+qeqe6789@users.noreply.github.com>
2022-02-01 15:44:43 +0900

    수정 기본틀

--- oasys/src/Components/WorkingSection/LeftControlPanel.js
+++ oasys/src/Components/WorkingSection/LeftControlPanel.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styled, { css } from "styled-components";
 import HighlightAltIcon from "@mui/icons-material/HighlightAlt";
 import PanToolIcon from "@mui/icons-material/PanTool";
@@ -36,16 +36,112 @@ const ResizedIconStyle = {
   margin: "4px 0",
   // 크기
   // 1rem = 브라우저 16px
-  fontSize: "1.5rem",
+  fontSize: "5rem",
   cursor: "pointer",
+
+  "&:hover":{
+    color : 'lightgreen',
+    backgroundColor: 'lightgray',
+  },
 };
 
+const ClickedIconStyle = {
+  // 색상
+  color: "lightgreen",
+  backgroundColor: 'lightgray',
+  border: "2px solid black",
+  borderRadius: "3px",
+  // 배치
+  margin: "4px 0",
+  // 크기
+  // 1rem = 브라우저 16px
+  fontSize: "5rem",
+  cursor: "pointer",
+
+  "&:hover":{
+    color : 'azure',
+    backgroundColor: 'black'
+  },
+};
+
+const StyledButton = styled.button`
+  border: 2px solid greenyellow;
+  border-radius: 4px;
+  background-color: azure;
+  margin: 16px;
+`;
+
 function LeftControlPanel({ areaPercent, ...rest }) {
+  const [panStyle , setPanStyle] = useState(ResizedIconStyle);
+  const [highStyle, setHighStyle] = useState(ResizedIconStyle);
+  const [polyStyle, setPolyStyle] = useState(ResizedIconStyle);
+  const [mode, setMode] = useState(null)
+
+  const onPanClick = () => {
+    if (panStyle === ResizedIconStyle) {
+      setPanStyle(ClickedIconStyle);
+      setMode("Pan");
+      if (highStyle === ClickedIconStyle) {
+        setHighStyle(ResizedIconStyle);
+      }
+      else if (polyStyle === ClickedIconStyle) {
+        setPolyStyle(ResizedIconStyle);
+      }
+    }
+    else{
+      setPanStyle(ResizedIconStyle);
+      setMode(null);
+    }
+  };
+
+  const onHighClick = () => {
+    if (highStyle === ResizedIconStyle) {
+      setHighStyle(ClickedIconStyle);
+      setMode("High");
+      if (panStyle === ClickedIconStyle) {
+        setPanStyle(ResizedIconStyle);
+      }
+      else if (polyStyle === ClickedIconStyle) {
+        setPolyStyle(ResizedIconStyle);
+      }
+    }
+    else{
+      setHighStyle(ResizedIconStyle);
+      setMode(null);
+    }
+  }
+  
+  const onPolyClick = () => {
+    if (polyStyle === ResizedIconStyle) {
+      setPolyStyle(ClickedIconStyle);
+      setMode("Poly");
+      if (panStyle === ClickedIconStyle) {
+        setPanStyle(ResizedIconStyle);
+      }
+      else if (highStyle === ClickedIconStyle) {
+        setHighStyle(ResizedIconStyle);
+      }
+    }
+    else{
+      setPolyStyle(ResizedIconStyle);
+      setMode(null);
+    }
+  };
+
   return (
     <StyledLeftPanel areaPercent={areaPercent} {...rest}>
-      <PanToolIcon sx={ResizedIconStyle} />
-      <HighlightAltIcon sx={ResizedIconStyle} />
-      <PolylineIcon sx={ResizedIconStyle} />
+      <PanToolIcon sx={panStyle} 
+        onClick = {onPanClick}
+      />
+      <HighlightAltIcon sx={highStyle} 
+        onClick = {onHighClick}
+      />
+      <PolylineIcon sx={polyStyle} 
+        onClick = {onPolyClick}
+      />
+      <StyledButton>
+        <h1>current Mode: {mode}</h1>
+      </StyledButton>
     </StyledLeftPanel>
   );
 }
