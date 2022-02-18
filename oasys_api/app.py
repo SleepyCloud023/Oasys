f68d251ecf726ec11a494dec670d54c61ee8c167
Deplim <hibyby2000@naver.com>
2022-02-18 16:13:27 +0000

    dataset api 추가

--- oasys_api/app.py
+++ oasys_api/app.py
@@ -4,50 +4,116 @@ from flask_cors import CORS, cross_origin
 import sqlalchemy as db
 import json
 
+# flask app
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
-
 CORS(app, resources={r'*': {'origins': ['*']}})
+
+# db config
+db_config = {
+    'user': 'root',
+    'password': '3376',
+    'host': 'localhost',
+    'port': 3306,
+    'database': 'OasysDB'
+}
+DB_URL = f"mysql+mysqlconnector://{db_config['user']}:{db_config['password']}@{db_config['host']}:{db_config['port']}/{db_config['database']}?charset=utf8"
+
 
 @app.route('/')
 def hello_oasys():
     return 'Hello, Oasys! (oasys api server)'
 
-@app.route('/image_info/<id>', methods = ['GET'])
+
+@app.route('/image_info/<id>', methods=['GET'])
 def get_object(id):
-    db_config = {
-        'user'     : 'root', 		# 1)
-        'password' : '3376', 		# 2)
-        'host'     : 'localhost', 	# 3)
-        'port'     : 3306, 			# 4)
-        'database' : 'OasysDB'		# 5)
-    }
-    DB_URL = f"mysql+mysqlconnector://{db_config['user']}:{db_config['password']}@{db_config['host']}:{db_config['port']}/{db_config['database']}?charset=utf8"
+    """특정 image 에 대한 정보 반환
 
-    engine = db.create_engine(DB_URL, encoding = 'utf-8', max_overflow = 0)
+    Args:
+        id (_type_): _description_
 
+    Returns:
+        _type_: _description_
+    """
+
+    # db connect
+    engine = db.create_engine(DB_URL, encoding='utf-8', max_overflow=0)
     connection = engine.connect()
     metadata = db.MetaData()
-    table = db.Table('annotation_object', metadata, autoload = True, autoload_with = engine)
 
-
-    # select * from table 과 같음
+    # make query object
+    table = db.Table('annotation_object', metadata,
+                     autoload=True, autoload_with=engine)
     query = db.select([table]).where(table.columns.id == id)
 
-    # 쿼리 실행
+    # execute query
     result_proxy = connection.execute(query)
     result_set = result_proxy.fetchall()
 
-    # 결과 print 이때 10개만 출력하도록 함. 단순한 set 자료구조의 형태를 하고 있음.
-    anno_json=result_set[0]["annotation"]
+    # get query result
+    anno_json = result_set[0]["annotation"]
     anno_json_object = json.loads(anno_json)
     imageURL = result_set[0]["image_url"]
     imageName = result_set[0]["image_name"]
     imageSize = result_set[0]["image_size"]
 
-    result_json = {"imageURL": imageURL, "imageName": imageName, "imageSize": imageSize, "annotation": anno_json_object}
-
+    # *** make result json ***
+    result_json = {"imageURL": imageURL, "imageName": imageName,
+                   "imageSize": imageSize, "annotation": anno_json_object}
+    # http response : application/json
     return jsonify(result_json)
 
+
+@app.route('/dataset/<id>', methods=['GET'])
+def get_dataset(id):
+    """특정 dataset에 대한 정보 반환
+
+    Args:
+        id (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+
+    # db connect
+    engine = db.create_engine(DB_URL, encoding='utf-8', max_overflow=0)
+    connection = engine.connect()
+    metadata = db.MetaData()
+
+    # *** make result json format ***
+    result_json = {"datasetName": "", "objects": []}
+
+    # make query object : dataset table
+    dataset_table = db.Table(
+        'dataset', metadata, autoload=True, autoload_with=engine)
+    query = db.select([dataset_table]).where(dataset_table.columns.id == id)
+    # execute query
+    result_proxy = connection.execute(query)
+    result_set = result_proxy.fetchall()
+    # get query result : datasetName
+    result_json["datasetName"] = result_set[0]["name"]
+
+    # make query object : annotation_object table
+    object_table = db.Table('annotation_object', metadata,
+                            autoload=True, autoload_with=engine)
+    query = db.select([object_table]).where(
+        object_table.columns.dataset_id == id)
+    # execute query
+    result_proxy = connection.execute(query)
+    result_set = result_proxy.fetchall()
+    # get query result : image info of dataset
+    for object in result_set:
+        objectID = object["id"]
+        imageURL = object["image_url"]
+        imageName = object["image_name"]
+        imageSize = object["image_size"]
+
+        result_json["objects"].append(
+            {"id": objectID, "imageURL": imageURL, "imageName": imageName, "imageSize": imageSize})
+
+    # http response : application/json
+    return jsonify(result_json)
+
+
 if __name__ == '__main__':
-    app.run(host = '0.0.0.0', port = 5000, debug = True) 
+    app.run(host='0.0.0.0', port=5000, debug=True)
