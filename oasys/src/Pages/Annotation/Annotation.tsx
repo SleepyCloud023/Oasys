703d4b41ac04ec2491c0f6dd6163088a36f9038e
SleepyCloud023 <gnflwhf352@gmail.com>
2022-04-01 03:22:46 +0900

    feat: Alert component & Annotation page alert manager

--- oasys/src/Pages/Annotation/Annotation.tsx
+++ oasys/src/Pages/Annotation/Annotation.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from '@emotion/styled';
 import { useParams } from 'react-router-dom';
 import Header from './Header/Header';
 import WorkingSection from './WorkingSection/WorkingSection';
 import Footer from './Footer/Footer';
+import AlertBox, { AlertInfo } from '../../Components/Alert/AlertBox';
 
 const AnnotationBlock = styled.div`
   display: flex;
@@ -12,17 +13,31 @@ const AnnotationBlock = styled.div`
   height: 100vh;
 `;
 
+const defaultAlert: AlertInfo = {
+  open: false,
+  success: true,
+  message: '',
+};
+
 function Annotation() {
-  const parmas = useParams();
-  let id = parmas.id;
-  if (id == null) {
-    id = '1';
-  }
+  const [alert, setAlert] = useState(defaultAlert);
+
+  const params = useParams();
+  let id: string = params.id || '1';
+
+  const AlertManger = () => (
+    <AlertBox
+      {...alert}
+      sx={{ position: 'absolute', top: '70px', margin: 'auto' }}
+      onCloseIcon={() => setAlert(defaultAlert)}
+    />
+  );
 
   return (
     <AnnotationBlock>
+      <AlertManger />
       <Header />
-      <WorkingSection id={parseInt(id)} />
+      <WorkingSection id={parseInt(id)} setAlert={setAlert} />
       <Footer />
     </AnnotationBlock>
   );
